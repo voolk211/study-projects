e3610aeeb5cc54063832af8b7907b8bf58115c95
voolk211 <fate6002@gmail.com>
2025-03-06 12:25:28 +0300

    Add files via upload

--- truthtable.py
+++ truthtable.py
@@ -1,17 +1,14 @@
-n=3
-nn=2
-values=[1]*n
-for i in range(1,n+1):
-    values[nn]=2**i//2
-    nn-=1
 
-
-length=2**n
-for i in range(0,length):
-    s=[0]*n
-    for j in range(0,n):
-        if(schetchik)
-        s[j]=values[j]
-
-        values[j]
-
+n=50
+s=[i for i in range(2,n+1)]
+answ=[]
+for i in range(0,len(s)):
+    if (s[i]!=0):
+        answ.append(s[i])
+    tmp=s[i]
+    j=i
+    if(tmp!=0):
+        while(j<len(s)):
+            s[j]=0;
+            j+=tmp
+print(answ)
